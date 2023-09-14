dce6ca96a6c07eb62724a088b16432400ec2889b
JuanOspinaECI <juan.ospina-s@mail.escuelaing.edu.co>
2023-09-13 22:46:42 -0500

    Recepcion de xyz y Frec cardiaca

--- Estacion_recepcion/Estacion_recepcion.ino
+++ Estacion_recepcion/Estacion_recepcion.ino
@@ -1,33 +1,4 @@
-/**
-   ESPNOW - Basic communication - Slave
-   Date: 26th September 2017
-   Author: Arvind Ravulavaru <https://github.com/arvindr21>
-   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
-   Description: This sketch consists of the code for the Slave module.
-   Resources: (A bit outdated)
-   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
-   b. http://www.esploradores.com/practica-6-conexion-esp-now/
 
-   << This Device Slave >>
-
-   Flow: Master
-   Step 1 : ESPNow Init on Master and set it in STA mode
-   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
-   Step 3 : Once found, add Slave as peer
-   Step 4 : Register for send callback
-   Step 5 : Start Transmitting data from Master to Slave
-
-   Flow: Slave
-   Step 1 : ESPNow Init on Slave
-   Step 2 : Update the SSID of Slave with a prefix of `slave`
-   Step 3 : Set Slave in AP mode
-   Step 4 : Register for receive callback and wait for data
-   Step 5 : Once data arrives, print it in the serial monitor
-
-   Note: Master and Slave have been defined to easily understand the setup.
-         Based on the ESPNOW API, there is no concept of Master and Slave.
-         Any devices can act as master or salve.
-*/
 
 #include <esp_now.h>
 #include <WiFi.h>
@@ -43,9 +14,7 @@ const char* ssid = "Apnea_device";
 const char* password = "holahola";
 
 // The String below "webpage" contains the complete HTML code that is sent to the client whenever someone connects to the webserver
-String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Sensors information</h1><p>X: <span id='x_acel'>-</span></p><p>Y: <span id='y_acel'>-</span></p><p>Z: <span id='z_acel'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { var msg = {brand: 'Gibson',type: 'Les Paul Studio',year: 2010,color: 'white'};Socket.send(JSON.stringify(msg)); } function processCommand(event) {var obj = JSON.parse(event.data);document.getElementById('x_acel').innerHTML = obj.x;document.getElementById('y_acel').innerHTML = obj.y;document.getElementById('z_acel').innerHTML = obj.z; console.log(obj.x_acel);console.log(obj.y_acel);console.log(obj.z_acel); } window.onload = function(event) { init(); }</script></html>";
-
-
+String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Sensors information</h1><p>X: <span id='x_acel'>-</span></p><p>Y: <span id='y_acel'>-</span></p><p>Z: <span id='z_acel'>-</span></p><p>beatsPerMinute: <span id='beatsPerMinute_'>-</span></p><p>beatAvg: <span id='beatAvg_'>-</span></p><p>irValue: <span id='irValue_'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span> <h2>Cuestionario STOP BANG</h2> <form action='#' method='post'> <label for='ronquidos'>1. Ronquidos:</label><br> Usted ronca fuerte? (lo suficiente para ser escuchado a traves de puertas cerradas o la persona con la que duerme lo mueve por sus ronquidos.)<br> <input type='radio' id='ronquidos_si' name='ronquidos' value='SI'> <label for='ronquidos_si'>SI</label> <input type='radio' id='ronquidos_no' name='ronquidos' value='NO'> <label for='ronquidos_no'>NO</label><br><br> <label for='cansancio'>2. Cansancio:</label><br> Se siente usted frecuentemente cansado, fatigado o somnoliento durante el día? (Por ejemplo se queda usted dormido mientras conduce o habla con alguien)<br> <input type='radio' id='cansancio_si' name='cansancio' value='SI'> <label for='cansancio_si'>SI</label> <input type='radio' id='cansancio_no' name='cansancio' value='NO'> <label for='cansancio_no'>NO</label><br><br> <label for='observacion'>3. Observacion:</label><br> Alguien lo ha observado dejar de respirar o ahogarse/jadear durante el suenno?<br> <input type='radio' id='observacion_si' name='observacion' value='SI'> <label for='observacion_si'>SI</label> <input type='radio' id='observacion_no' name='observacion' value='NO'> <label for='observacion_no'>NO</label><br><br> <label for='presion_arterial'>4. Presion Arterial:</label><br> Tiene o está siendo tratado por hipertension arterial?<br> <input type='radio' id='presion_arterial_si' name='presion_arterial' value='SI'> <label for='presion_arterial_si'>SI</label> <input type='radio' id='presion_arterial_no' name='presion_arterial' value='NO'> <label for='presion_arterial_no'>NO</label><br><br> <label for='imc'>5. Indice de Masa Corporal mayor a 35 kg/m2:</label><br> Su Indice de Masa Corporal (BMI) es mayor a 35 kg/m2? (Calculado como peso en kilogramos dividido por la altura en metros al cuadrado)<br> <input type='radio' id='imc_si' name='imc' value='SI'> <label for='imc_si'>SI</label> <input type='radio' id='imc_no' name='imc' value='NO'> <label for='imc_no'>NO</label><br><br> <label for='edad'>6. Mayor de 50 annos:</label><br> Tiene usted más de 50 annos?<br> <input type='radio' id='edad_si' name='edad' value='SI'> <label for='edad_si'>SI</label> <input type='radio' id='edad_no' name='edad' value='NO'> <label for='edad_no'>NO</label><br><br> <label for='cuello'>7. Tamaño del cuello (Medido alrededor de la manzana de Adan):</label><br> Es el cuello de su camisa de 16 pulgadas / 40 cm o mas grande?<br> <input type='radio' id='cuello_si' name='cuello' value='SI'> <label for='cuello_si'>SI</label> <input type='radio' id='cuello_no' name='cuello' value='NO'> <label for='cuello_no'>NO</label><br><br> <label for='genero'>8. Genero:</label><br> Hombre<br> <input type='radio' id='genero_hombre' name='genero' value='Hombre'> <label for='genero_hombre'>SI</label><br> <input type='radio' id='genero_mujer' name='genero' value='Mujer'> <label for='genero_mujer'>NO</label><br><br> <input type='submit' value='Enviar'> </form></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { var msg = {brand: 'Gibson',type: 'Les Paul Studio',year: 2010,color: 'white'};Socket.send(JSON.stringify(msg)); } function processCommand(event) {var obj = JSON.parse(event.data);document.getElementById('x_acel').innerHTML = obj.x;document.getElementById('y_acel').innerHTML = obj.y;document.getElementById('z_acel').innerHTML = obj.z;document.getElementById('beatsPerMinute_').innerHTML = obj.beatsPerMinute;document.getElementById('beatAvg_').innerHTML = obj.beatAvg;document.getElementById('irValue_').innerHTML = obj.irValue; console.log(obj.x_acel);console.log(obj.y_acel);console.log(obj.z_acel); } window.onload = function(event) { init(); }</script></html>";
 // We want to periodically send values to the clients, so we need to define an "interval" and remember the last time we sent data to the client (with "previousMillis")
 int interval = 1000;                                  // send data to the client every 1000ms -> 1s
 unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
@@ -57,6 +26,9 @@ typedef struct struct_message {
     float y;
     float z;
     float temp;
+    int beatsPerMinute;
+    int beatAvg;
+    int irValue;
     int readingId;
 } struct_message;
 
@@ -64,6 +36,8 @@ typedef struct struct_message {
 struct_message incomingReadings;
 
 JSONVar board;
+
+
 
 // Initialization of webserver and websocket
 WebServer server(80);                                 // the server uses port 80 (standard port for websites
@@ -141,7 +115,15 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
     board["z"] = incomingReadings.y;
     board["readingId"] = String(incomingReadings.readingId);
     String jsonString = JSON.stringify(board);
-  } else {
+  } else if(incomingReadings.id == 3){
+    board["id"] = incomingReadings.id;
+    board["beatsPerMinute"] = incomingReadings.x;
+    board["beatAvg"] = incomingReadings.beatAvg;
+    board["irValue"] = incomingReadings.irValue;
+    board["readingId"] = String(incomingReadings.readingId);
+    String jsonString = JSON.stringify(board);
+  }
+  else {
     board["id"] = incomingReadings.id;
     board["temp"] = incomingReadings.temp;
     board["readingId"] = String(incomingReadings.readingId);
@@ -156,6 +138,15 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
     Serial.printf(" y: %4.2f \n", incomingReadings.y);
     Serial.printf(" z: %4.2f \n", incomingReadings.z);
     axes_data(incomingReadings.x,incomingReadings.y,incomingReadings.z);
+  }
+  else if(incomingReadings.id == 3){
+    heart_data(incomingReadings.beatsPerMinute, incomingReadings.beatAvg, incomingReadings.irValue);
+    Serial.printf(" BPM: ");
+    Serial.println(incomingReadings.beatsPerMinute);
+    Serial.printf(" BPM_AVG: ");
+    Serial.println(incomingReadings.beatAvg);
+    Serial.printf(" IRVALUE: ");
+    Serial.println(incomingReadings.z);
   }
   else { Serial.printf(" temp: %4.2f \n", incomingReadings.y);}
   Serial.printf("readingID value: %d \n", incomingReadings.readingId);
@@ -187,13 +178,27 @@ void loop() {
 }
 
 void axes_data (float x_, float y_, float z_){
-      
-    String jsonString = "";                           // create a JSON string for sending data to the client
-    StaticJsonDocument<200> doc;                      // create a JSON container
-    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
+      String jsonString = "";                           // create a JSON string for sending data to the client
+StaticJsonDocument<200> doc;                      // create a JSON container
+JsonObject object = doc.to<JsonObject>();
+
+            // create a JSON Object
     object["x"] = x_;                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
     object["y"] = y_;
     object["z"] = z_;
+    //Serial.println(object.);
+    serializeJson(doc, jsonString);                   // convert JSON object to string
+    //Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
+    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
+}
+
+void heart_data(float beatsPerMinute_, int beatAvg_, long irValue_){
+  String jsonString = "";                           // create a JSON string for sending data to the client
+StaticJsonDocument<200> doc;                      // create a JSON container
+JsonObject object = doc.to<JsonObject>();
+    object["beatsPerMinute"] = beatsPerMinute_;                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+    object["beatAvg"] = beatAvg_;
+    object["irValue"] = irValue_;
     serializeJson(doc, jsonString);                   // convert JSON object to string
     //Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
     webSocket.broadcastTXT(jsonString);               // send JSON string to clients
